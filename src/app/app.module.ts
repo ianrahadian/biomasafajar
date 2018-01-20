8562740fdf598716fb970d9dbd6a825162d69377
ianrahadian <v4llcrist@gmail.com>
2018-01-20 19:15:43 +0700

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
